4f5094bf54081afd0975c3174fa38fa87f732dd9
rezvan farahani <rezvan.farahani@outlook.com>
2019-07-08 12:03:53 +0430

    Create Tuition model

---
+++ Host/Model/TuitionModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Host.Model
+{
+    public class TuitionModel
+    {
+        public int Amount { get; set; }
+        public String Title { get; set; }
+    }
+}
